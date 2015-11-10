fa018c2496bd3c5b2f4d7a5c2a323564ab977af1
sdmurff <sdmurff@users.noreply.github.com>
2015-11-09 17:21:17 -0700

    Create File

---
+++ HW2.R
@@ -0,0 +1,4 @@
+#Read in Data
+M <- read.table('http://web.stanford.edu/~xing/statfinbook/_BookData/Chap03/m_sp500ret_3mtcm.txt', skip=1, header=T)
+
+X <- read.table('http://web.stanford.edu/~xing/statfinbook/_BookData/Chap03/m_ret_10stocks.txt', header=T)
